9a9cd62c86533b286a53b3a81e4ab0a1c5e7a3be
Ronan Avila <avila.ronan@gmail.com>
2022-09-05 14:11:00 -0300

    WIP

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+
+            <Avatar hasBorder={false} src="https://github.com/ronanavila.png"/>
+
+            <div className={styles.commentBox}>
+
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>José Manuel</strong>
+                            <time title="05 de setembro de 2022 às 01:01h" dateTime="2022-09-05 01:01:00">Cerca de 1 hora </time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+                    <p>Muito bom</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post(){
@@ -5,7 +7,7 @@ export function Post(){
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/ronanavila.png"/>
+                    <Avatar src="https://github.com/ronanavila.png"/>
                     <div className={styles.authorInfo}>
                         <strong>Paulo José</strong>
                         <span>Web Developer</span>
@@ -22,6 +24,19 @@ export function Post(){
                 <a href="">#maisum</a>{' '}
                 <a href="">#legal</a>
                 </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea placeholder="Deixe seu comentário"/>
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+            <div className={styles.commentList}>
+                <Comment/>
+                <Comment/>
+                <Comment/>
             </div>
         </article>
     );
